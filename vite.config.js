2965575a008b1290a6476b14b41ba83296b3830b
anjan-sarkar46 <Marshallaw600@gmail.com>
2025-04-28 19:45:06 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,35 +6,31 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      buffer: 'buffer',
-      process: 'process/browser',
+      './runtimeConfig': './runtimeConfig.browser',
       util: 'util',
       stream: 'stream-browserify',
-      './runtimeConfig': './runtimeConfig.browser',
-    },
+    }
   },
   define: {
     'process.env.NODE_DEBUG': 'false',
-    global: 'window',
+    global: 'globalThis'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    },
+    include: ['util', 'stream-browserify']
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
     rollupOptions: {
       external: ['aws-crt'],
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           aws: ['@aws-sdk/client-s3', '@aws-sdk/lib-storage']
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['buffer', 'process/browser'],
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
+        }
       }
     }
   }
